934882a258b9c56b5a09ffd913dfb13df9315bbb
matsumana07384 <manami.m.227@outlook.jp>
2018-08-21 21:49:11 +0900

    add js

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,30 @@
   var app = new Vue({
     el: '#app',
+    //アプリケーションで使用するデータ
     data: {
       message: 'Vue.js!',
+      inputdata:'初期値',
       list:['りんご','ばなな','いちご'],
-      show: true
+      showdata: true,
+      show: true,
     },
+    //算出プロパティ
+    computed:{
+        computedMessage: function(){
+          return this.message+'!'
+        }
+    },
+    // ライフサイクルフック
+    created:function() {
+      //行いたい処理
+      
+    },
+    // アプリケーションで使用するメソッド
     methods:{
       handleClick: function(event){
-        alert(event.target)//[object HTMLButtonElement]
+        //行いたい処理
+        alert(event.target)
+        //[object HTMLButtonElement]
       }
     }
   });
